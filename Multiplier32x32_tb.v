9a82c22bed6d48840f6e3846c39cfe38183e3b54
José Augusto dos Santos Lemos <d2022004055@unifei.edu.br>
2025-06-07 20:57:22 -0300

    Update Multiplier32x32_tb.v

--- Multiplier32x32_tb.v
+++ Multiplier32x32_tb.v
@@ -4,26 +4,21 @@ module Multiplier32x32_tb();
 
     reg [31:0] A;
     reg [31:0] B;
-	 reg clk;
     wire [63:0] Product;
 
     Multiplier32x32 uut (
         .A(A),
         .B(B),
-		  .clk(clk),
         .Product(Product)
     );
 	 
-	 always #5 clk = ~clk;
-	 
     initial begin
 	 
-		  clk = 0;
         A = 32'hFFFFFFFF;
         B = 32'hFFFFFFFF;
 		  
-		  #100;
-		  $finish;
+	#100;
+	$finish;
 		  
     end
 
